4a869b041d6884fc3e56c193508ce071c7ba3063
spradeep055 <spradeep055@gmail.com>
2024-06-27 16:19:54 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -9,17 +9,26 @@
 
 int StringCalculator::add(const std::string& strinput)
 {
+  int strlen = stringinput
 if( strinput.empty())
 {
   std::cout<<"Input String is NULL"<<std::endl;
   return 0;
 }
 
+  if(checkstringiszero(strinput) == 0)
+  {
+     std::cout<<"Input String is NULL"<<std::endl;
+     return 0;
+  }
+
+}
+
+int checkstringiszero(std::string& strinput)
+{
 if( strinput.size() == 1 && strinput[0] == '0')
 {
   std::cout << "False" << std::endl;
   return 0;
 }
-
 }
-
