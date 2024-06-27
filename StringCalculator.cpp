bbdda155fad63881b41c0a743b30faab159ca262
spradeep055 <spradeep055@gmail.com>
2024-06-27 15:14:28 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,16 +1,22 @@
 #include "StringCalculator.h"
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdexcept>
+#include <regex>
+#include <algorithm>
 
 class StringCalculator{
 
 
-int add(std::string str)
+int add(std::string strinput)
 {
-if( str == NULL)
+if( strinput.empty() == NULL)
 {
   cout<<"Input String is NULL"<<endl;
   return 0;
 }
-if(str == "0")
+if(strinput == "0")
 {
   cout<<Input string is NULL<<endl;
 }
